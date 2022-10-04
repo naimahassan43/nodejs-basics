b401ea1942a510d989672c0cf98eb87d44e5b04d
Naima Hassan <naima.hassan434@gmail.com>
2022-10-04 12:25:44 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const amount = 10
+
+if (amount < 10) {
+  console.log('smaller number')
+} else if (amount == 10) {
+  console.log('equal number')
+} else {
+  console.log('larger number')
+}
+
+console.log(`'Hi, it's my first nodejs!!'`)
